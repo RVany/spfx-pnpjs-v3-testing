9d460ed4bae5dc82ea15386fe01b9fa79b26dc12
MoonSky1! <vanmathyrr@gmail.com>
2024-03-18 21:41:09 -0400

    first commit

---
+++ src/webparts/spfxPnpjsV3Testing/components/ISpfxPnpjsV3TestingProps.ts
@@ -0,0 +1,7 @@
+export interface ISpfxPnpjsV3TestingProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/spfxPnpjsV3Testing/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISpfxPnpjsV3TestingWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SpfxPnpjsV3TestingWebPartStrings' {
+  const strings: ISpfxPnpjsV3TestingWebPartStrings;
+  export = strings;
+}
